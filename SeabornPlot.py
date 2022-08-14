cc78ae225092b6a4079e26a0c7c70fd26f43287d
John Ervin <49054998+StJohnErvin@users.noreply.github.com>
2022-08-14 15:12:44 +0800

    Create SeabornPlot.py

---
+++ SeabornPlot.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('filename.csv')
+
+df.head
+
+g = sns.barplot()
+
+## create your table contents and style
+
+plt.show()
